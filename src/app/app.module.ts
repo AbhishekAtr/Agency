683afd99c21d624d40a9df9e2b30f75a5f1d82c2
Abhishek Verma <abhishekverma339@gmail.com>
2022-04-21 12:56:48 +0530

    add new pages and Pages UI done and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,44 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { Routes, RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { FooterComponent } from './footer/footer.component';
+import { AutomationComponent } from './automation/automation.component';
+import { PopupComponent } from './popup/popup.component';
+import { MarketingComponent } from './marketing/marketing.component';
+import { WebsiteDevelopmentComponent } from './website-development/website-development.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { OurPortfolioComponent } from './our-portfolio/our-portfolio.component';
+
+const appRoutes: Routes=[
+  {path:'', component: HomeComponent},
+  {path:'automation', component: AutomationComponent},
+  {path:'marketing', component: MarketingComponent},
+  {path:'website-development', component: WebsiteDevelopmentComponent},
+  {path:'about-us', component: AboutUsComponent},
+  {path:'our-portfolio', component: OurPortfolioComponent},
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     HomeComponent,
-    FooterComponent
+    FooterComponent,
+    AutomationComponent,
+    PopupComponent,
+    MarketingComponent,
+    WebsiteDevelopmentComponent,
+    AboutUsComponent,
+    OurPortfolioComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule. forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
